832f1f8144e6ba67ba5f76f3df7621a14cfb4184
margie1a <4987kk@naver.com>
2025-01-26 02:44:40 +0900

    [feat]: JWT 토큰을 사용할 수 있도록 스웨거 명세서에 요청 헤더 구현

--- demo/src/main/java/BodyBuddy/demo/global/config/SwaggerConfig.java
+++ demo/src/main/java/BodyBuddy/demo/global/config/SwaggerConfig.java
@@ -1,11 +1,13 @@
 package BodyBuddy.demo.global.config;
 
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
@@ -14,15 +16,27 @@ public class SwaggerConfig {
 	public OpenAPI BodyBuddyAPI() {
 		// API 정보 설정
 		Info info = new Info()
-			.title("BodyBuddy API")
-			.description("BodyBuddy 프로젝트의 API 명세서입니다.")
-			.version("1.0.0");
+				.title("BodyBuddy API")
+				.description("BodyBuddy 프로젝트의 API 명세서입니다.")
+				.version("1.0.0");
+
+		// JWT Security Scheme 설정
+		SecurityScheme securityScheme = new SecurityScheme()
+				.type(SecurityScheme.Type.HTTP)
+				.scheme("bearer")
+				.bearerFormat("JWT")
+				.in(SecurityScheme.In.HEADER)
+				.name("Authorization");
+
+		// Security Requirement 설정
+		SecurityRequirement securityRequirement = new SecurityRequirement()
+				.addList("bearerAuth");
 
 		// OpenAPI 설정 반환
 		return new OpenAPI()
-			.addServersItem(new Server().url("/")) // 기본 서버 URL 설정
-			.info(info)
-			.components(new Components()); // SecuritySchemes 없이 설정
+				.addServersItem(new Server().url("/")) // 기본 서버 URL 설정
+				.info(info)
+				.components(new Components().addSecuritySchemes("bearerAuth", securityScheme)) // SecuritySchemes 추가
+				.addSecurityItem(securityRequirement); // 모든 API에 SecurityRequirement 추가
 	}
-
 }

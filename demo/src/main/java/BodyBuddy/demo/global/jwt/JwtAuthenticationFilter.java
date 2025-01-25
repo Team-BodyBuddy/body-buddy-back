b3f0992e83401a3dab6585021380369ee154b637
margie1a <4987kk@naver.com>
2025-01-26 02:38:09 +0900

    [feat]: JWT 인증/인가 설정 파일들 구현

---
+++ demo/src/main/java/BodyBuddy/demo/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,40 @@
+package BodyBuddy.demo.global.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            var authentication = jwtTokenProvider.getAuthentication(token);
+
+            if (authentication != null && authentication instanceof UsernamePasswordAuthenticationToken) {
+                ((UsernamePasswordAuthenticationToken) authentication)
+                        .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}

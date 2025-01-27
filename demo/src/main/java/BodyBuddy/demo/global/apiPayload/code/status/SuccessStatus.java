120a2f99d6a75b9d04a857a39ec1630e1dbbe9a4
margie1a <4987kk@naver.com>
2025-01-27 19:50:17 +0900

    [feat]: 공통 성공응답 상태코드 추가

--- demo/src/main/java/BodyBuddy/demo/global/apiPayload/code/status/SuccessStatus.java
+++ demo/src/main/java/BodyBuddy/demo/global/apiPayload/code/status/SuccessStatus.java
@@ -12,7 +12,28 @@ import lombok.Getter;
 public enum SuccessStatus implements BaseCode {
 
 	// 일반적인 응답
-	_OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+	_OK(HttpStatus.OK, "COMMON200", "성공입니다."),
+
+	// 로그인 관련 응답
+	LOGIN_SUCCESS(HttpStatus.OK, "AUTH2000", "로그인 성공입니다."),
+
+	// 회원가입 관련 응답
+	SIGNUP_SUCCESS(HttpStatus.OK,"AUTH2010", "회원가입 성공입니다."),
+
+	// 회원 탈퇴 관련 응답
+	DELETE_ACCOUNT_SUCCESS(HttpStatus.OK,"AUTH2020", "회원 탈퇴 성공입니다."),
+
+	// 랭킹 관련 응답
+	RANKINGSUCCESS(HttpStatus.OK, "RANKING2000", "랭킹 조회 성공입니다."),
+	RANKINGUPDATE(HttpStatus.OK, "RANKING2001", "랭킹 보너스 업데이트 성공입니다."),
+	RANKINGSCOREUPDATE(HttpStatus.OK, "RANKING2002", "랭크 점수 업데이트 성공입니다."),
+
+	//헬스장 관련 응답
+	GYMSUCCESS(HttpStatus.OK, "GYM200", "체육관 조회 성공입니다."),
+
+	//지역 관련 응답
+	REGIONSUCCESS(HttpStatus.OK, "REGION200", "지역 조회 성공입니다.");
+
 
 	private final HttpStatus httpStatus;
 	private final String code;

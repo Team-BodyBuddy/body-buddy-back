f30140e1acfc410b9e8a32d43ae838afa1a7121d
Dongjun <ldj990517@gmail.com>
2025-02-01 05:25:26 +0900

    [FEAT] 공통예외처리 BodyBuddyException 만들었어요.

---
+++ demo/src/main/java/BodyBuddy/demo/global/apiPayload/code/error/MemberErrorCode.java
@@ -0,0 +1,23 @@
+package BodyBuddy.demo.global.apiPayload.code.error;
+
+import org.springframework.http.HttpStatus;
+
+import BodyBuddy.demo.global.apiPayload.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberErrorCode implements ErrorCode {
+
+	//예시입니다.
+
+	// ✅ 회원 관련 오류
+	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_001", "해당 회원을 찾을 수 없습니다."),
+	DUPLICATE_NICKNAME(HttpStatus.BAD_REQUEST, "MEMBER_002", "이미 사용 중인 닉네임입니다."),
+	EMAIL_ALREADY_EXISTS(HttpStatus.BAD_REQUEST, "MEMBER_003", "이미 가입된 이메일입니다.");
+
+	private final HttpStatus httpStatus;
+	private final String code;
+	private final String message;
+}

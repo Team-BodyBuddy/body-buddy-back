3dcdd260171233b7e5368c44e6073532ef60c4d3
MINSOO CHOI <4987kk@naver.com>
2025-02-03 13:08:15 +0900

    [fix]: CORS 수정

--- demo/src/main/java/BodyBuddy/demo/global/config/CorsMvcConfig.java
+++ demo/src/main/java/BodyBuddy/demo/global/config/CorsMvcConfig.java
@@ -10,10 +10,10 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000, https://body-buddy.aoimiu.com") // 허용된 출처
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용된 HTTP 메서드
-                .allowedHeaders("*") // 모든 요청 헤더 허용
-                .exposedHeaders("Authorization", "Content-Type") // 클라이언트에 노출할 헤더
-                .allowCredentials(true); // 쿠키 또는 인증 헤더 허용
+                .allowedOrigins("http://localhost:3000", "https://body-buddy.aoimiu.com") // 허용된 도메인 추가
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(true);
     }
 }

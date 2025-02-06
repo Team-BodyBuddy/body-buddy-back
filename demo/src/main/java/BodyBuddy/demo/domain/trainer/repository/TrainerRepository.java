93275ff110234ddc3a9a86f2d1dc4ae2f1f146b4
margie1a <4987kk@naver.com>
2025-02-06 18:33:26 +0900

    [feat]: 회원(트레이너, 일반유저) 레포지토리 리프레시 토큰 업데이트 추가

--- demo/src/main/java/BodyBuddy/demo/domain/trainer/repository/TrainerRepository.java
+++ demo/src/main/java/BodyBuddy/demo/domain/trainer/repository/TrainerRepository.java
@@ -1,6 +1,8 @@
 package BodyBuddy.demo.domain.trainer.repository;
 
 import BodyBuddy.demo.domain.trainer.entity.Trainer;
+import BodyBuddy.demo.global.apiPayload.code.error.TrainerErrorCode;
+import BodyBuddy.demo.global.apiPayload.exception.BodyBuddyException;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,11 @@ import java.util.Optional;
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
     Optional<Trainer> findByLoginId(String loginId);
     boolean existsByLoginId(String loginId);
+
+    default void updateRefreshToken(String loginId, String refreshToken) {
+        Trainer trainer = findByLoginId(loginId)
+            .orElseThrow(() -> new BodyBuddyException(TrainerErrorCode.TRAINER_NOT_FOUND));
+        trainer.setRefreshToken(refreshToken);
+        save(trainer);
+    }
 }

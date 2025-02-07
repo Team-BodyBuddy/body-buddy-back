5c57f1658e692f1abb4d6208b9d730f6e34271bb
margie1a <4987kk@naver.com>
2025-02-07 17:18:55 +0900

    [fix]: swagger 설정

--- demo/src/main/java/BodyBuddy/demo/global/config/SwaggerConfig.java
+++ demo/src/main/java/BodyBuddy/demo/global/config/SwaggerConfig.java
@@ -17,13 +17,11 @@ public class SwaggerConfig {
 
 	@Bean
 	public OpenAPI BodyBuddyAPI(Environment environment) {
-		// API 정보 설정
 		Info info = new Info()
 			.title("BodyBuddy API")
 			.description("BodyBuddy 프로젝트의 API 명세서입니다.")
 			.version("1.0.0");
 
-		// JWT Security Scheme 설정
 		SecurityScheme securityScheme = new SecurityScheme()
 			.type(SecurityScheme.Type.HTTP)
 			.scheme("bearer")
@@ -31,7 +29,6 @@ public class SwaggerConfig {
 			.in(SecurityScheme.In.HEADER)
 			.name("Authorization");
 
-		// Security Requirement 설정
 		SecurityRequirement securityRequirement = new SecurityRequirement()
 			.addList("bearerAuth");
 

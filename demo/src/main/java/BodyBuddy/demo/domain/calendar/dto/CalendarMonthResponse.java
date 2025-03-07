c30abb863eaec0fb5d4ea8b0a7174661b2a7e3b4
Dongjun <ldj990517@gmail.com>
2025-03-07 21:49:47 +0900

    getEvaluationStatus 추가

--- demo/src/main/java/BodyBuddy/demo/domain/calendar/dto/CalendarMonthResponse.java
+++ demo/src/main/java/BodyBuddy/demo/domain/calendar/dto/CalendarMonthResponse.java
@@ -3,13 +3,16 @@ package BodyBuddy.demo.domain.calendar.dto;
 import java.time.LocalDate;
 
 import BodyBuddy.demo.domain.calendar.entity.Calendar;
+import BodyBuddy.demo.global.common.commonEnum.EvaluationStatus;
 
-public record CalendarMonthResponse(Long calendarId, LocalDate date, CalendarDayInfo.IndicatorType indicatorType) {
+public record CalendarMonthResponse(Long calendarId, LocalDate date, CalendarDayInfo.IndicatorType indicatorType,
+									EvaluationStatus evaluationStatus) {
 	public static CalendarMonthResponse from(Calendar calendar){
 		return new CalendarMonthResponse(
 			calendar.getId(),
 			calendar.getDate(),
-			calendar.getIndicator()
+			calendar.getIndicator(),
+			calendar.getEvaluationStatus()
 		);
 	}
 
